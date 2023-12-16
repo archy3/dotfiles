2795bffaa3ecf20d335bcc3f3c36ca78133fb3d1
archy3 <26029938+archy3@users.noreply.github.com>
2023-12-16 15:35:50 -0600

    vm-resize: update xrandr based on events
    Stop calling xrandr repeatedly, because now each xrandr call
    fills up ~/.local/share/xorg/Xorg.0.log with garbage.

--- scripts/vm-resize.sh
+++ scripts/vm-resize.sh
@@ -1,27 +1,22 @@
 #!/bin/sh
 main()
 {
+  # From https://stackoverflow.com/a/2173421
+  trap 'trap - TERM; kill -s TERM -- -"$$"' INT TERM QUIT EXIT
+
   spice-vdagent &
 
   output=$(xrandr | awk '/connected/ && ! /disconnected/ {print $1}')
 
-  wallpaper="${HOME}/downloads/deadly-wallpaper.jpg"
-  readjust "$output" "$wallpaper"
-
-  while sleep 1; do
-    resolution_list="$(xrandr)"
-    if [ "${resolution_list%'*+'*}" = "${resolution_list}" ]; then
-      readjust "$output" "$wallpaper"
-    fi
-  done
-}
-
-# Just running this without checking first if the resolution list
-# changed seems to actually be more efficient.
-readjust() ## <output> <wallpaper>
-{
-  xrandr --output "$1" --auto
-  xwallpaper --zoom "$2"
+  # From https://superuser.com/a/1565544
+  # Running the below code in the background and waiting
+  # allows this script to still response to signals.
+  xev -root -event randr |
+    grep --line-buffered 'subtype XRROutputChangeNotifyEvent' |
+      while read -r pointlessvar; do
+        xrandr --output "$output" --auto
+      done &
+  wait "$!"
 }
 
 main "$@"
