60ee993451d93b0a6ef6d562f884756d5af2b6be
archy3 <26029938+archy3@users.noreply.github.com>
2024-05-12 22:45:08 -0500

    fix unclean shutdown false positive on first boot

--- scripts/autostart/unclean-shutdown-check.sh
+++ scripts/autostart/unclean-shutdown-check.sh
@@ -1,26 +1,30 @@
 #!/bin/sh
 
+# See https://ww.reddit.com/r/ChrisTitusTech/comments/abtp56/easiest_way_to_check_last_shutdown_was_clean/ed38elw/
 main()
 {
   set -euf
   trap '[ "$?" != 0 ] && printf \\n%s\\n "${0}: An error occurred." >&2' EXIT
 
-  if last_shutdown_was_clean; then
-    printf %s\\n "Last shutdown appears clean."
-  else
-    msg="Last shutdown appears dirty"
-    printf %s\\n "${msg}."
-    notify-send --expire-time=10000 --urgency=critical -- "${msg}"
-  fi
+  # If this value is 2, then that means there was
+  # no (proper) shutdown between the last two bootups:
+  number_of_bootups_in_last_two_shutdown_or_bootup_events=$(
+    last -x |
+      awk '($1=="reboot" && $2=="system" && $3=="boot") || ($1=="shutdown" && $2=="system" && $3=="down")' |
+      head -2 |
+      { grep -c -e '^reboot[[:blank:]][[:blank:]]*system boot' || true; }
+  )
+
+  case "$number_of_bootups_in_last_two_shutdown_or_bootup_events" in
+    2) notify "Last shutdown appears dirty";;
+    *) printf %s\\n "Last shutdown appears clean.";;
+  esac
 }
 
-# See https://ww.reddit.com/r/ChrisTitusTech/comments/abtp56/easiest_way_to_check_last_shutdown_was_clean/ed38elw/
-last_shutdown_was_clean()
+notify()
 {
-  last -x |
-    grep -e 'system boot' -e 'system down' |
-    head -2 |
-    grep -q 'system down'
+  printf %s\\n "${1}."
+  notify-send --expire-time=10000 --urgency=critical -- "${1}"
 }
 
 main "$@"
