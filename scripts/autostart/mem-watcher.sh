5a7e978bb0209122e91c088b5377d1881b707cae
archy3 <26029938+archy3@users.noreply.github.com>
2024-06-22 18:01:40 -0500

    mem-watcher: display remaining free memory in notification

--- scripts/autostart/mem-watcher.sh
+++ scripts/autostart/mem-watcher.sh
@@ -5,43 +5,74 @@ main()
   set -euf
   trap '[ "$?" != "0" ] && printf \\n%s\\n "${0}: An error occurred." >&2' EXIT
 
-  total_mem=$(free | awk '/Mem:/ {print $2}')
-  low_mem=$((total_mem*15/100))
-  very_low_mem=$((total_mem*10/100))
-  critically_low_mem=$((total_mem*5/100))
+  # Random positive int (no leading zeros)
+  notification_id=$(
+    < /dev/urandom tr -dc 123456789 | head -c 1
+    < /dev/urandom tr -dc 0123456789 | head -c 8
+  )
 
-  nl="
-  "
-  nl="${nl%%' '*}"; nl="${nl%%'	'*}"
+  while true; do
+    # shellcheck disable=SC2046
+    set -- $(get_mem_info)
+    mem_available_percent="$1"
+    mem_available="${2}${3}"
 
-  low_mem_msg="Low on RAM:${nl}Kill applications to free up RAM"
-  very_low_mem_msg="Very low on RAM:${nl}Kill applications quickly to free up RAM"
-  critically_low_mem_msg="CRITICALLY LOW ON RAM!!!${nl}KILL APPLICATIONS IMMEDIATELY!"
-
-  while sleep 5; do
-    available_mem=$(free | awk '/Mem:/ {print $7}')
-
-    if [ "$available_mem" -lt "$critically_low_mem" ]; then
-      warn_low_mem "$critically_low_mem_msg"
-    elif [ "$available_mem" -lt "$very_low_mem" ]; then
-      warn_low_mem "$very_low_mem_msg"
-    elif [ "$available_mem" -lt "$low_mem" ]; then
-      warn_low_mem "$low_mem_msg"
+    if [ "$mem_available_percent" -le 15 ]; then
+      warn_low_mem "Low on RAM (${mem_available} free)" "$notification_id"
     fi
+
+    sleep 5
   done
 }
 
-warn_low_mem() ## warning_message
+warn_low_mem() # <warning message> <notification id>
 {
   if command -v notify-send > /dev/null; then
     notify-send \
       --hint=int:transient:1 \
+      --replace-id="$2" \
       --expire-time=10000 \
       --urgency=critical \
       -- "$1"
   fi
+
   printf %s\\n "$1"
-  sleep 10
+}
+
+# Output is "<mem available percent> <mem available> <mem available units>"
+get_mem_info()
+{
+  awk \
+  '
+    function print_kb_int_in_human_readable_form(kb) {
+      if (kb < 1000) {
+        value = kb
+        suffix = "KB"
+      }
+      else if (kb < 1024 * 1000) {
+        value = kb / 1024
+        suffix = "MB"
+      }
+      else {
+        value = kb / 1024 / 1024
+        suffix = "GB"
+      }
+
+      printf "%0.1f %s\n", value, suffix
+    }
+
+    {
+      gsub(":", "", $1)
+      meminfo[$1] = $2
+    }
+
+    END {
+      mem_avail_percent = 100 * meminfo["MemAvailable"] / meminfo["MemTotal"]
+
+      printf "%s ", int(mem_avail_percent)
+      print_kb_int_in_human_readable_form(meminfo["MemAvailable"])
+    }
+  ' /proc/meminfo
 }
 
 main "$@"
