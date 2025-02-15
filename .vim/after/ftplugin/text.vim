archy3/dotfiles

commit e7b9230abfdeeabb1ab4a3de4268d6a68db403bc

vim: fix "b:undo_ftplugin not taking effect" bug
61982a30dd74e5b4b12432178b15ba40301d43e7 introduced a bug where
`b:undo_ftplugin` would not take effect when switching to a different
filetype in some situations (github issue #1). This fixes that,
though is somewhat hacky (and it's still not clear why the bug showed
up in the first place).

.vim/after/ftplugin/text.vim
@@ -1,9 +1,24 @@
-setlocal spell
+setlocal spelllang=en,es
 setlocal wrap
 setlocal linebreak
 
+" We want spellcheck on new files
+" (e.g. vim just opened without opening a file, opened a file that doesn't exist,
+" opened an empty file, or an `enew`):
+" (from https://vi.stackexchange.com/a/2559):
+if ((@% == "") || (filereadable(@%) == 0) || (line('$') == 1 && col('$') == 1))
+  setlocal spell
+
+  " For some reason, `b:undo_ftplugin` isn't activated in this case which
+  " leaves settings like `spell` still active when a new filetype is entered.
+  " Thus we make an autocmd to set the filetype back to `text` and then
+  " reset it back to whatever it was before to activate the `b:undo_ftplugin'.
+  autocmd BufNewFile,BufRead * let s:filetype_save = &filetype | setlocal filetype=text | exec 'setlocal filetype=' . s:filetype_save
+endif
+
 if exists('b:undo_ftplugin')
   let b:undo_ftplugin .= '|setlocal spell<'
+  let b:undo_ftplugin .= '|setlocal spelllang<'
   let b:undo_ftplugin .= '|setlocal wrap<'
   let b:undo_ftplugin .= '|setlocal linebreak<'
 endif

.vim/ftdetect/text.vim
@@ -1,0 +1,15 @@
+" If no filetype detected, set it to text
+" (from https://vi.stackexchange.com/a/13293):
+autocmd BufNewFile,BufRead * if &filetype ==# '' | set filetype=text | endif
+
+" Make this also work when opening vim without a file,
+" when opening a new file (that doesn't exist),
+" when opening an empty file (that exists),
+" and when doing stuff like `enew`.
+" (from https://vi.stackexchange.com/a/2559):
+autocmd VimEnter * if ((&filetype ==# '') && ((@% == "") || (filereadable(@%) == 0) || (line('$') == 1 && col('$') == 1))) | set filetype=text | endif
+  " For some reason `b:undo_ftplugin` isn't invoked in this case when
+  " chaining to a new filetype.
+  " A fix is provided in ~/.vim/after/ftplugin/text.vim
+
+

.vim/init/autocmd.vim
@@ -179,16 +179,3 @@ function! HelpProperLocation()
   "}}}
 endfunc
 "}}}
-
-" If no filetype detected, set it to text
-" (from https://vi.stackexchange.com/a/13293):
-" {{{
-  augroup NoFileType
-    autocmd!
-    autocmd BufNewFile,BufRead * if &filetype ==# '' | setlocal filetype=text | endif
-  augroup END
-
-  " Make this work even when vim starts without a file
-  " (from https://vi.stackexchange.com/a/2559):
-  autocmd vimenter * if @% == "" | setlocal filetype=text | endif
-" }}}

commit 67253541f9fe92c7731a91694ef48c88045c38cf

vim: text: fix performance problems
e7b9230abfdeeabb1ab4a3de4268d6a68db403bc introduced a performance
regression that caused some plugins (like vimtex when repeatedly
exiting and then re-entering a quickfix window) to perform
extremely poorly (slowly) (particularly when the opening/closing
of buffers/windows was involved). This fixes the regression by
only resetting the filetype when it seems the situation that
requires it to be reset has occurred.

.vim/after/ftplugin/text.vim
@@ -15,7 +15,11 @@ if ((@% == "") || (filereadable(@%) == 0) || (line('$') == 1 && col('$') == 1))
   " leaves settings like `spell` still active when a new filetype is entered.
   " Thus we make an autocmd to set the filetype back to `text` and then
   " reset it back to whatever it was before to activate the `b:undo_ftplugin'.
-  autocmd BufNewFile,BufRead * let s:filetype_save = &filetype | setlocal filetype=text | exec 'setlocal filetype=' . s:filetype_save
+  let w:text_ftplugin_may_need_to_be_undone = 1
+  autocmd BufNewFile,BufRead * if exists('w:text_ftplugin_may_need_to_be_undone') | let s:filetype_save = &filetype | set filetype=text | exec 'set filetype=' . s:filetype_save | unlet w:text_ftplugin_may_need_to_be_undone | endif
+    " Without the guard `if exists('w:text_ftplugin_may_need_to_be_undone')`,
+    " performance problems can occur in plugins that create
+    " many buffers (such as quick-fix windows).
 endif
 
 if exists('b:undo_ftplugin')