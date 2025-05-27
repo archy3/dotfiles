66072b96c4b394742e1d7a536230a97ccb85b1ae
archy3 <26029938+archy3@users.noreply.github.com>
2025-05-26 19:56:56 -0500

    pcmanfm: disable mouse-forward/back workaround on gtk3 build
    Only the gtk2 version of pcmanfm requires this workaround to
    make the mouse forward/back buttons work as expected.

--- scripts/pcmanfm-forward-back.sh
+++ scripts/pcmanfm-forward-back.sh
@@ -17,7 +17,15 @@ main() #<Left|Right>
   class="$(xprop -id "$id" WM_CLASS)"
 
   case "${class##*' '}" in
-    '"Pcmanfm"') xdotool key --clearmodifiers -- "$1";;
+    '"Pcmanfm"') run_if_on_debian_12_or_less "$@";;
+  esac
+}
+
+# gtk3 pcmanfm (debian 13 and later) does not need this workaround:
+run_if_on_debian_12_or_less()
+{
+  case "$(awk -F . -- '{print $1}' /etc/debian_version)" in
+    [5-9]|1[0-2]) xdotool key --clearmodifiers -- "$1";;
   esac
 }
 
