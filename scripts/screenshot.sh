c66179d520b4e29bff52424548ea8d4984e541b6
archy3 <26029938+archy3@users.noreply.github.com>
2026-02-07 16:53:00 -0600

    screenshot: add way to screenshot directly to clipboard

--- scripts/screenshot.sh
+++ scripts/screenshot.sh
@@ -1,14 +1,17 @@
 #!/bin/sh
 
 # TODO:
-# Process fulscreen images on multimonitor setups to split the image
+# Process fullscreen images on multimonitor setups to split the image
 # into one separate image per monitor.
-# Give nsxiv a crop command (longterm).
 
 # Takes a screenshot with maim and then opens the screenshot with nsxiv.
-# The first and only argument should be <f|s|S|F|c|C>.
+# The arguments should be [-clipboardonly] <f|s|S|F|c|C>.
 
 # Argument meanings:
+# -clipboardonly: Save the image to the clipboard instead of permanently saving
+#                 to the disk (though a temp file will briefly be created).
+#                 nsxiv will not be invoked.
+#
 # f: fullscreen
 # s: select with decorations
 # S: select without decorations
@@ -20,19 +23,66 @@
 # The cursor capturing options have a delay to allow enough time to
 # position the cursor to the desired location.
 
-date="$(date +%Y-%m-%d-%T)" || return "$?"
-screenshot="${HOME}/pictures/screenshots/${date}.png"
-delay="5.0"
+# Global variables:
+Screenshot=''
+Clipboardonly=false
 
-case "$1" in
-  f) maim --hidecursor --window=root -- "$screenshot";;
-  s) maim --hidecursor --select --color=1,0,0,0.6 -- "$screenshot";;
-  S) maim --hidecursor --nodecorations --select --color=1,0,0,0.6 -- "$screenshot";;
-  F) maim --delay="$delay" --window=root -- "$screenshot";;
-  c) maim --delay="$delay" --select --color=1,0.5,0,0.6 -- "$screenshot";;
-  C) maim --delay="$delay" --nodecorations --select --color=1,0.5,0,0.6 -- "$screenshot";;
-esac || return "$?"
+main()
+{
+  set -euf
+  trap '[ "$?" != 0 ] && print_error_message; cleanup' EXIT
 
-if test -f "$screenshot"; then
-  nsxiv -b -- "$screenshot"
-fi
+  format=png
+  date="$(date +%Y-%m-%d-%T)"
+  Screenshot="${HOME}/pictures/screenshots/${date}.${format}"
+  delay='5.0'
+  color_no_cursor='1,0,0,0.6'
+  color_cursor='1,0.5,0,0.6'
+
+  case "$1" in
+    -clipboardonly) Clipboardonly=true; shift;;
+  esac
+
+  screenshot_type="$1"
+
+  if [ "$Clipboardonly" = true ]; then
+    Screenshot="$(mktemp)"
+    color_no_cursor='0,1,0,0.6'
+    color_cursor='1,1,0,0.6'
+  fi
+
+  case "$screenshot_type" in
+    f) maim --format="$format" --hidecursor --window=root -- "$Screenshot";;
+    s) maim --format="$format" --hidecursor --select --color="$color_no_cursor" -- "$Screenshot";;
+    S) maim --format="$format" --hidecursor --nodecorations --select --color="$color_no_cursor" -- "$Screenshot";;
+    F) maim --format="$format" --delay="$delay" --window=root -- "$Screenshot";;
+    c) maim --format="$format" --delay="$delay" --select --color="$color_cursor" -- "$Screenshot";;
+    C) maim --format="$format" --delay="$delay" --nodecorations --select --color="$color_cursor" -- "$Screenshot";;
+  esac
+
+  if [ -f "$Screenshot" ] && [ -s "$Screenshot" ]; then
+    if [ "$Clipboardonly" = true ]; then
+      xclip -selection clipboard -t image/png < "$Screenshot"
+
+      case "$screenshot_type" in
+        c|C|f|F) notify-send --icon=camera-photo -- 'Screenshot taken!';;
+      esac
+    else
+      nsxiv -b -- "$Screenshot" &
+    fi
+  fi
+}
+
+cleanup()
+{
+  if [ "$Clipboardonly" = true ] && [ -f "$Screenshot" ]; then
+    rm -- "$Screenshot"
+  fi
+}
+
+print_error_message()
+{
+  printf \\n%s\\n "${0}: An error occurred." >&2
+}
+
+main "$@"
