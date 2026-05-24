6cdca1dee436a5f2f5132c59be44ffe09c5f3eb5
archy3 <26029938+archy3@users.noreply.github.com>
2026-05-23 19:42:57 -0500

    keyboard layout: fix keyboard not using Hyper_R
    When xmodmap replaces Super_R with Hyper_R right after X is started, the
    change doesn't always take effect until the Super_R/Hyper_R physical key
    is pressed (which will send the Super_R keysym the first time, but will
    then send the Hyper_R keysym on all subsequent presses).
    We simulate pressing the Super_R/Hyper_R physical key after executing
    xmodmap so that Hyper_R will be sent even the first time the user
    physically presses the Super_R/Hyper_R physical key (though the first
    physical press will still sometimes result in Super_R being sent if the
    user presses the Super_R/Hyper_R physical key within the first second of
    X having been launched).

--- scripts/autostart/set-keyboard-layout-on-usb-connect.sh
+++ scripts/autostart/set-keyboard-layout-on-usb-connect.sh
@@ -5,7 +5,7 @@ main()
   set -euf
   trap '[ "$?" != 0 ] && printf \\n%s\\n "${0}: An error occurred." >&2' EXIT
 
-  set_keyboard_layout
+  set_keyboard_layout "${1:-}"
 
   if [ "${1:-}" = '--set-layout-now-and-exit' ]; then
     return 0
@@ -15,6 +15,9 @@ main()
   # (we want the line to be word-split, so no `IFS= ` before `read`)
   udevadm monitor -k -s hidraw | while read -r _ _event_type __ ___; do
     case "${_event_type:-}" in
+      # We don't pass `"${1:-}"` to `set_keyboard_layout`
+      # because the "press hyper" trick seems to be only
+      # necessary once.
       add|remove) set_keyboard_layout;;
     esac
   done
@@ -23,7 +26,7 @@ main()
   return 1
 }
 
-set_keyboard_layout()
+set_keyboard_layout() # [--send-hyper]
 {
   # Make right alt the compose key.
   setxkbmap -layout us -option '' -option compose:ralt
@@ -43,10 +46,23 @@ set_keyboard_layout()
     -e 'keycode  206 = NoSymbol  Super_R   NoSymbol  Super_R' \
     -e 'keycode  108 = Multi_key Multi_key Multi_key Multi_key'
 
+  # For some reason the changes don't always take effect if
+  # the Hyper_R keys has not been subsequently depressed:
+  if [  "${1:-}" = "--send-hyper" ]; then
+    xdotool key --clearmodifiers -- Hyper_R
+  fi
+
   # When the above xmodmap command is called repeatedly with
   # no time between calls, sometimes the keymap reverts to the default.
   # This short sleep is an inelegant way to combat that.
   sleep 1
+
+  # Sometimes sending the `Hyper_R` doesn't work
+  # when it's issued too close to issuing `xmodmap`,
+  # so we'll send it again:
+  if [  "${1:-}" = "--send-hyper" ]; then
+    xdotool key --clearmodifiers -- Hyper_R
+  fi
 }
 
 main "$@"
